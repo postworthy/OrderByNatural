21b70aa86770eb6bf738aa89aca4d8af310cdd76
Landon Key <jolkey@JOLKEY15.southernco.com>
2016-02-11 08:14:03 -0600

    First Commit

---
+++ OrderByNatural/OrderByNaturalExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace com.LandonKey.OrderByNatural
+{
+    public static class OrderByNaturalExtensions
+    {
+        public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> objects, Func<T, string> func)
+        {
+            Func<string, object> convert = str =>
+            {
+                int x = 0;
+                if (int.TryParse(str, out x))
+                    return x;
+                else
+                    return str;
+            };
+
+            return objects.OrderBy(x =>
+                Regex.Split(func(x), "([0-9]+)").Select(convert),
+                new EnumerableComparer<object>());
+        }
+
+        public static IEnumerable<T> OrderByNaturalDesc<T>(this IEnumerable<T> objects, Func<T, string> func)
+        {
+            return objects.OrderByNatural(func).Reverse();
+        }
+    }
+}
---
+++ OrderByNatural/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OrderByNatural")]
+[assembly: AssemblyDescription("Use this set of extension methods for ordering text in a more natural way.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Landon Key")]
+[assembly: AssemblyProduct("OrderByNatural")]
+[assembly: AssemblyCopyright("Copyright © Landon Key 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1962c41d-ca58-43f7-bcfd-a323f641fc1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
